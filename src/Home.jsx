db43bb2e08ae16a17a012dd232ec177f85ce502d
GeffersonParra <yefparra1@soy.sena.edu.co>
2026-01-18 20:25:03 -0500

    Subida 4

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,47 @@
+import { motion } from "framer-motion";
+
 export const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center px-30 h-[94vh]">
       <div className="h-full w-full flex justify-center items-center relative">
         <div className="text-center my-auto ml-auto">
-          <p className="text-9xl font-jersey text-[#FF0000]">EL VIKINGO</p>
-          <p className="font-yorkshire text-black text-8xl">
-            Rodillos y Herramientas
-          </p>
-          <div className="flex gap-8 justify-center">
+          <motion.div
+            className="flex flex-col gap-4 justify-center btn-section"
+            initial={{ opacity: 0, x: -300 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+          >
+            <p className="text-9xl font-jersey text-[#FF0000]">EL VIKINGO</p>
+            <motion.div
+              className="flex flex-col gap-4 justify-center btn-section"
+              initial={{ opacity: 0, x: -300 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, ease: "easeOut" }}
+            >
+              <p className="font-yorkshire text-black text-8xl">
+                Rodillos y Herramientas
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.div
+            className="flex gap-4 justify-center btn-section"
+            initial={{ opacity: 0, y: 500 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.2, ease: "easeOut" }}
+          >
             <button className="btn-primary mt-12 font-jersey">Catalogo</button>
-            <button className="btn-secondary mt-12 font-jersey">Contacto</button>
-          </div>
+            <button className="btn-secondary mt-12 font-jersey">
+              Contacto
+            </button>
+          </motion.div>
         </div>
+        <motion.div 
+            initial={{ opacity: 0, x: 500 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+        >
         <img src="/image2.png" className="w-[36rem] my-auto mr-auto" alt="" />
+        </motion.div>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ export const Header = () => {
         <div class="container mx-auto px-6 py-4 flex justify-between items-center">
           <img src="/image1.png" className="w-24" alt="" />
           <div class="space-x-6">
-            <a href="#productos" class="hover:text-blue-600">
+            <a href="#productos" class="hover:text-[#FF0000]">
               Productos
             </a>
-            <a href="#contacto" class="hover:text-blue-600">
+            <a href="#contacto" class="hover:text-[#FF0000]">
               Contacto
             </a>
           </div>
