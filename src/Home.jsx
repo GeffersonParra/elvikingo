83fc78df984a5ead591b89b494fffed3b42f73a7
GeffersonParra <yefparra1@soy.sena.edu.co>
2026-01-18 20:06:30 -0500

    Subida 2

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,15 @@
 export const Home = () => {
     return (
         <div className="flex flex-col items-center justify-center px-30 h-[94vh]">
-            <div className="h-full w-full flex">
-                <div className="text-9xl font-jersey text-[#FF0000] text-center my-auto mx-auto">
-                    EL VIKINGO
+            <div className="h-full w-full flex justify-center items-center relative">
+                <div className="text-center my-auto ml-auto">
+                    <p className="text-9xl font-jersey text-[#FF0000]">EL VIKINGO</p>
+                    <p className="font-yorkshire text-black text-8xl">Rodillos y Herramientas</p>
+                    <button className="btn-primary mt-12 font-jersey">
+                        Catalogo
+                    </button>
                 </div>
-                <img src="/image2.png" className="w-[36rem] ml-auto my-auto" alt="" />
+                <img src="/image2.png" className="w-[36rem] my-auto mr-auto" alt="" />
             </div>
         </div>
     )
